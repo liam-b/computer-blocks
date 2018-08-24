51deb89acfdeaec066c9343361fa79865e6ea049
jkbriant <jkbriant@gmail.com>
2018-08-25 09:52:44 +1000

    i fixed it lol

--- src/computerblocks/block/Block.java
+++ src/computerblocks/block/Block.java
@@ -83,7 +83,7 @@ public class Block {
     }
   }
 
-  private void highlightBlock(Display display, Player player, float size, RealPosition position) {
+  public void highlightBlock(Display display, Player player, float size, RealPosition position) {
     float highlightOffset = BLOCK_HIGHLIGHT_OFFSET * player.zoom;
     display.color(new Color("#31c831"));
     display.rect(
