7af35b4f1cb902bbcb29247b459461e9da494872
Liam Brennan <liam@anywhich.com>
2018-11-12 12:49:11 +1100

    made grid bigger

--- scripts/mac_run.sh
+++ scripts/mac_run.sh
@@ -1,6 +1,6 @@
 cd src/
 javac -cp '.:lib/json.jar' $1.java
 if [[ $? == 0 ]]; then
-  java -cp '.:lib/json.jar' $1
+  java -Dsun.java2d.opengl=true -cp '.:lib/json.jar' $1
 fi
 find . -name "*.class" -type f -delete
